943257a13ed515f938a19d8748f23d7ba7d16a15
Aynur Galiev <aynurgaliev@mail.ru>
2022-08-23 23:08:34 +0300

    Added 15. 3Sum

--- LeetCodeSandbox/Solutions/15. 3Sum.swift
+++ LeetCodeSandbox/Solutions/15. 3Sum.swift
@@ -11,16 +11,38 @@ enum ThreeSum {
     
     class Solution {
         func threeSum(_ nums: [Int]) -> [[Int]] {
+            var result = [[Int]]()
+            var map: [Int: [Int]] = [:]
+            let numbers = nums.sorted()
+            for i in 0..<numbers.count {
+                map[numbers[i], default: []].append(i)
+            }
             
-            return []
+            for i in 0..<numbers.count - 1 {
+                for j in i + 1..<numbers.count {
+                    if let indexes = map[-(numbers[i] + numbers[j])], indexes.last! > j {
+                        result.append([numbers[i], numbers[j], -(numbers[i] + numbers[j])])
+                    }
+                }
+            }
+            
+            return Array(Set(result))
         }
     }
 }
 
 extension ThreeSum.Solution: SolutionProtocol {
     var testCases: [Validatable] {
+        let comparator: ([[Int]]) -> Set<[Int]> = { result in
+            var set = Set<[Int]>()
+            result.forEach { set.insert($0.sorted()) }
+            return set
+        }
+        
         return [
-            TestCase1(input1: [-1,0,1,2,-1,-4], result: [[-1,-1,2],[-1,0,1]], func1: self.threeSum),
+            TestCase1(input1: [-1,0,1,2,-1,-4], result: [[-1,0,1], [-1,-1,2]], func1: self.threeSum, comparator: comparator),
+            TestCase1(input1: [0, 1, 1], result: [], func1: self.threeSum, comparator: comparator),
+            TestCase1(input1: [0, 0, 0], result: [[0, 0, 0]], func1: self.threeSum, comparator: comparator)
         ]
     }
 }
--- LeetCodeSandbox/Utilities/TestCase.swift
+++ LeetCodeSandbox/Utilities/TestCase.swift
@@ -9,8 +9,11 @@ import Foundation
 
 protocol TestCase: Validatable {
     associatedtype Result: Equatable
+    associatedtype ComparableResult: Equatable
+    
     var result: Result { get }
     var `func`: () -> Result { get }
+    var comparator: (Result) -> ComparableResult { get }
 }
 
 protocol Validatable {
@@ -19,37 +22,90 @@ protocol Validatable {
 
 extension TestCase {
     func validate() {
-        let actualResult = self.func()
-        print("[\(self.result == actualResult ? "SUCCESS" : "FAILURE")]: Expected - \(self.result), actual - \(actualResult)")
+        let startTime = clock()
+        let result = self.func()
+        let endTime = clock()
+        let milliseconds = (Double(endTime - startTime) / Double(CLOCKS_PER_SEC)) * 1000
+        
+        let numberFormatter = NumberFormatter()
+        numberFormatter.maximumFractionDigits = 2
+        numberFormatter.decimalSeparator = "."
+        let value = numberFormatter.string(from: milliseconds as NSNumber)!
+        
+        let actualResult = self.comparator(result)
+        let expectedResult = self.comparator(self.result)
+        
+        print("[\(value) ms][\(expectedResult == actualResult ? "SUCCESS" : "FAILURE")]: Expected - \(expectedResult), actual - \(actualResult)")
     }
 }
 
-struct TestCase0<Result: Equatable>: TestCase {
+struct Measure {
+    
+    enum Time {
+        case milliseconds
+        case seconds
+    }
+    
+    static func run(_ closure: () -> Void, time: Time = .milliseconds) -> Int {
+        let startTime = clock()
+        closure()
+        let endTime = clock()
+        let elapsedTimeInSeconds = Double(endTime - startTime) / Double(CLOCKS_PER_SEC)
+        
+        switch time {
+            case .milliseconds: return Int(elapsedTimeInSeconds * 1000)
+            case .seconds: return Int(elapsedTimeInSeconds)
+        }
+    }
+}
+
+struct TestCase0<Result: Equatable, ComparableResult: Equatable>: TestCase {
     let result: Result
     let func0: () -> Result
+    let comparator: (Result) -> ComparableResult
     
     var `func`: () -> Result {
         { self.func0() }
     }
 }
 
-struct TestCase1<Input1, Result: Equatable>: TestCase {
+extension TestCase0 where Result == ComparableResult {
+    init(result: Result, func0: @escaping () -> Result) {
+        self.init(result: result, func0: func0, comparator: { result in result })
+    }
+}
+
+struct TestCase1<Input1, Result: Equatable, ComparableResult: Equatable>: TestCase {
     let input1: Input1
     let result: Result
     let func1: (Input1) -> Result
+    let comparator: (Result) -> ComparableResult
     
     var `func`: () -> Result {
         { self.func1(self.input1) }
     }
 }
 
-struct TestCase2<Input1, Input2, Result: Equatable>: TestCase {
+extension TestCase1 where Result == ComparableResult {
+    init(input1: Input1, result: Result, func1: @escaping (Input1) -> Result) {
+        self.init(input1: input1, result: result, func1: func1, comparator: { result in result })
+    }
+}
+
+struct TestCase2<Input1, Input2, Result: Equatable, ComparableResult: Equatable>: TestCase {
     let input1: Input1
     let input2: Input2
     let result: Result
     let func2: (Input1, Input2) -> Result
+    let comparator: (Result) -> ComparableResult
     
     var `func`: () -> Result {
         { self.func2(self.input1, self.input2) }
+    }
+}
+
+extension TestCase2 where Result == ComparableResult {
+    init(input1: Input1, input2: Input2, result: Result, func2: @escaping (Input1, Input2) -> Result) {
+        self.init(input1: input1, input2: input2, result: result, func2: func2, comparator: { result in result })
     }
 }
