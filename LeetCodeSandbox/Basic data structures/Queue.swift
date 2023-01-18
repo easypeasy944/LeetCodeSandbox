7fc5e86f362c24d6034f6f9e1a0e52c209e8391c
Aynur Galiev <aynurgaliev@mail.ru>
2023-01-18 17:53:35 +0300

    Added Queue

---
+++ LeetCodeSandbox/Basic data structures/Queue.swift
@@ -0,0 +1,53 @@
+//
+//  Queue.swift
+//  LeetCodeSandbox
+//
+//  Created by Aynur Galiev on 18.01.2023.
+//
+
+import Foundation
+
+struct Queue<T> {
+    class Node {
+        let val: T
+        weak var next: Node?
+        
+        init(val: T, next: Node? = nil) {
+            self.val = val
+            self.next = next
+        }
+    }
+    
+    private var head: Node? = nil
+    private var tail: Node? = nil
+    private(set) var count: Int = 0
+    private let size: Int
+    
+    init(size: Int) {
+        self.size = size
+    }
+    
+    mutating func push(_ value: T) {
+        if count == size {
+            head = head?.next
+        }
+        let newNode = Node(val: value)
+        
+        if head == nil {
+            head = newNode
+            tail = newNode
+        } else {
+            tail?.next = newNode
+            tail = newNode
+        }
+    }
+    
+    mutating func pop() -> T? {
+        let value = head?.val
+        head = head?.next
+        if head == nil {
+            tail = nil
+        }
+        return value
+    }
+}
